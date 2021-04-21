e0d4fb7cceee56f2a55d86825183015fc7c969dc
EPS-DEV <49179221+EliasSchramm@users.noreply.github.com>
2021-04-21 18:32:56 +0200

    Added timelinepoints (still some offset (still need to figure out why))

--- src/borderify.js
+++ src/borderify.js
@@ -1,13 +1,16 @@
 const list_url='https://api.eps-dev.de:42070/list/all';
 const detail_url='https://api.eps-dev.de:42070/get/?'
 
+const YT_BAR_COLOR = "#2f4858"
+const HIGHLIGHTIN_COLOR = "#FF0000"
+
 const STYLE = 
 `
 @import url("https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap");
 
 .eps{
-    background: #aca7cb;    
-
+    background: #aca7cb;   
+    
     padding:3%; 
     margin-bottom: 3%;     
     border-radius:5px;
@@ -35,6 +38,16 @@ const STYLE =
     margin: 0;
 }
 `;
+
+const YT_BAR_STYLE = 
+`
+.ytp-progress-bar{
+    background: rgb(2,0,36);
+    background: linear-gradient(to left, 
+        ?
+    );
+}
+`
 
 var HTML = 
 `
@@ -97,21 +110,36 @@ function isLivestream(){
     })
 }
 
+function array_avg(array){
+    i = 0;
+
+    array.forEach(x => {
+        i += x
+    })
+
+    return i / array.length
+}
+
 function getStreamDetails(){
     return new Promise((resolve, reject) => {
         fetch(detail_url.replace("?", LAST_ID))
         .then(response => response.json())
         .then(data => {
-            console.log("REEEEEEEEE");
+
             var details = {
                 mgs_total: 0,
                 avg_mgs_pm: Math.round(data.avg_mgs_per_min),
                 lang: [],
-                lang_p: []
+                lang_p: [],
+                highlights: []
             }
 
             let total_identified = 0;
             let lang_map = new Map();
+
+            let first_msg = Date.parse(data.msg_times[0].time)
+            let length = -(first_msg - Date.parse(data.msg_times[data.msg_times.length- 1].time));
+            let last = [0,0,0,0,0,0,0,0,0,0]
 
             data.msg_times.forEach((i) => {
                 details.mgs_total += i.count;
@@ -126,7 +154,14 @@ function getStreamDetails(){
                         }
 
                         total_identified += v;
-                })                      
+
+                        if(i.count > array_avg(last) * 5){
+                            details.highlights.push((Date.parse(i.time) - first_msg))
+                        }
+
+                        last.push(i.count)
+                        last.shift()                        
+                })                 
             })             
 
             lang_map = new Map([...lang_map.entries()].sort((a, b) => b[1] - a[1]))
@@ -137,6 +172,23 @@ function getStreamDetails(){
             details.lang_p[1] = (details.lang_p[1]/total_identified*100).toPrecision(2)
             details.lang_p[2] = (details.lang_p[2]/total_identified*100).toPrecision(2)
 
+            let interpoled = []
+            let _last = 0
+
+            details.highlights.forEach((x) => {
+                console.log();
+                if((x - 1000 * 60) > _last){
+                    interpoled.push([(x - 1000 * 30)/length, (x + 1000 * 30)/length])
+                    _last = x;
+
+                    var date = new Date(0);
+                    date.setSeconds(x / 1000); // specify value for SECONDS here
+                    var timeString = date.toISOString().substr(11, 8);
+                    console.log(timeString)
+                }
+            })
+            details.highlights = interpoled;
+
             resolve(details)
         });
     })
@@ -145,6 +197,25 @@ function getStreamDetails(){
 function insertStyle(){
     let head = document.getElementsByTagName("head")[0]
     head.append(htmlToElement("<style>?</style>".replace("?", STYLE)))
+}
+
+function insertYT_BAR_STYLE(points){
+    console.log(points);
+    let gradient_config = ""
+    let last_endpoint = 0;
+    points.forEach(point => {
+        let startpoint = Math.max(0 , point[0]) * 100
+        let endpoint = Math.min(0.9999999999 , point[1]) * 100
+
+        gradient_config += YT_BAR_COLOR + " " + last_endpoint + "%" + " " + startpoint + "%, \n"
+        gradient_config += HIGHLIGHTIN_COLOR + " " + startpoint + "%" + " " + endpoint + "%, \n"
+        
+        last_endpoint = endpoint;
+    })
+
+    gradient_config += YT_BAR_COLOR + " " + last_endpoint + "%" + " 100%"
+    let head = document.getElementsByTagName("head")[0]
+    head.append(htmlToElement("<style>?</style>".replace("?", YT_BAR_STYLE.replace("?", gradient_config))))
 }
 
 
@@ -158,15 +229,16 @@ function remove(){
 function refresh(){
     if(getCurrentID() != LAST_ID || document.getElementsByClassName("eps").length === 0){        
         LAST_ID = getCurrentID()
-        console.log(LAST_ID);
+
         isLivestream().then((r) => {                
-            if(r) {                        
-                console.log("RERENDERING");                
+            if(r) {          
                 getStreamDetails().then((details) => { 
                     remove()
                     let secondary = document.getElementById("secondary-inner");
                     let ln = new Intl.DisplayNames(['en'], {type: 'language'});
-                    
+
+                    insertYT_BAR_STYLE(details.highlights)
+
                     let _HTML = HTML;
                     _HTML = _HTML.replace("$mgs_pm", details.avg_mgs_pm)
                     _HTML = _HTML.replace("$mgs_total", details.mgs_total)
